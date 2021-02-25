0483c093b7ebb913d2cb675fa19ad7990842913a
Jordan Mueller <jmueller@goldstarlearning.com>
2021-02-25 13:43:27 -0500

    Add seq attribute to course objects.
    This is used to order the courses in the document XML (previously was
            done by grade sequence)

--- lib/academic_benchmarks/standards/course.rb
+++ lib/academic_benchmarks/standards/course.rb
@@ -5,17 +5,18 @@ module AcademicBenchmarks
     class Course
       include InstVarsToHash
 
-      attr_accessor :guid, :description
+      attr_accessor :guid, :description, :seq
 
       alias_method :descr, :description
 
       def self.from_hash(hash)
-        self.new(description: hash["descr"], guid: hash["guid"])
+        self.new(description: hash["descr"], guid: hash["guid"], seq: hash["seq"])
       end
 
-      def initialize(guid:, description:)
+      def initialize(guid:, description:, seq:)
         @guid = guid
         @description = description
+        @seq = seq
       end
     end
   end
